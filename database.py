c107dc89a78108a803177d5cc6df094f09070b7d
Jean-Loïck <116283418+jeanloick@users.noreply.github.com>
2024-06-16 11:58:30 +0200

    first commit

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Ensure the DATABASE_URL is correctly set or fallback to a default value
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://talib:talibPass@localhost/items")
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a Base class for declarative models
+Base = declarative_base()
